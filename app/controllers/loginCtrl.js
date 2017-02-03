ff6732929fe287ece6e31f09d4a38619e78ac0d4
Peter Garrido <peter.garrido57@gmail.com>
2017-02-03 14:26:47 -0600

    styling changes

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -1,5 +1,5 @@
-app.controller('loginCtrl', function ($scope, $location, authFactory) {
+app.controller('loginCtrl', function($scope, $location, authFactory) {
     $scope.login = () => authFactory
-      .login($scope.user)
-      .then(() => $location.url('/news'))
-})
+        .login($scope.user)
+        .then(() => $location.url('/news'));
+});
